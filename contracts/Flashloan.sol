23215d94ee45520c4d84e34953e08c2d8e9091a7
Sanchit Gupta <sanchitgupta456@gmail.com>
2021-06-29 19:24:42 +0530

    Flash Loan Code Working

---
+++ contracts/Flashloan.sol
@@ -0,0 +1,44 @@
+pragma solidity ^0.6.6;
+
+import "./aave/FlashLoanReceiverBase.sol";
+import "./aave/ILendingPoolAddressesProvider.sol";
+import "./aave/ILendingPool.sol";
+
+contract Flashloan is FlashLoanReceiverBase {
+
+    constructor(address _addressProvider) FlashLoanReceiverBase(_addressProvider) public {}
+
+    /**
+        This function is called after your contract has received the flash loaned amount
+     */
+    function executeOperation(
+        address _reserve,
+        uint256 _amount,
+        uint256 _fee,
+        bytes calldata _params
+    )
+        external
+        override
+    {
+        require(_amount <= getBalanceInternal(address(this), _reserve), "Invalid balance, was the flashLoan successful?");
+
+        //
+        // Your logic goes here.
+        // !! Ensure that *this contract* has enough of `_reserve` funds to payback the `_fee` !!
+        //
+
+        uint totalDebt = _amount.add(_fee);
+        transferFundsBackToPoolInternal(_reserve, totalDebt);
+    }
+
+    /**
+        Flash loan. Pass in the ammount using 18 decimal places.
+        For example, to borrow 1 DAI, set amount to 1e18 (1000000000000000000).
+     */
+    function flashloan(address _asset, uint _amount) public onlyOwner {
+        bytes memory data = "";
+
+        ILendingPool lendingPool = ILendingPool(addressesProvider.getLendingPool());
+        lendingPool.flashLoan(address(this), _asset, _amount, data);
+    }
+}
